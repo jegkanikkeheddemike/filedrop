4e0166082539ac3bf2319c0ce6ce3c857d854656
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2023-08-12 21:39:18 +0200

    autostart

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 cargo build --release
 sudo cp ./target/release/filedrop_daemon /usr/bin/
-if ! grep -q "filedrop_daemon &" ~/.xinitrc; then
-    echo -e "\nfiledrop_daemon &\n" >> ~/.xinitrc
-fi
+#if ! grep -q "filedrop_daemon &" ~/.xinitrc; then
+#    echo -e "\nfiledrop_daemon &\n" >> ~/.xinitrc
+#fi
+mkdir -p ~/.config/autostart
+cp ./filedrop_daemon/filedrop_daemon.desktop ~/.config/autostart/
