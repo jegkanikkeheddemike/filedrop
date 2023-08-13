49bbbb7fd3bcf15901163ae100c058746a9dc5b3
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2023-08-13 17:31:20 +0200

    better install script

--- linux_install.sh
+++ linux_install.sh
@@ -3,9 +3,7 @@ cargo build --release
 sudo rm -f /usr/bin/filedrop*
 sudo cp ./target/release/filedrop_daemon /usr/bin/
 sudo cp ./target/release/filedrop /usr/bin/
-#if ! grep -q "filedrop_daemon &" ~/.xinitrc; then
-#    echo -e "\nfiledrop_daemon &\n" >> ~/.xinitrc
-#fi
+
 mkdir -p ~/.config/autostart
 cp ./filedrop_daemon/filedrop_daemon.desktop ~/.config/autostart/
 filedrop_daemon &
