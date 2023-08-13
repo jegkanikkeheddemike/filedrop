2be6c4add581ef7486815d2753d931b2bdaf8105
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2023-08-13 15:49:17 +0200

    windos

--- filedrop_daemon/src/notifier.rs
+++ filedrop_daemon/src/notifier.rs
@@ -4,21 +4,31 @@ use tokio::sync::oneshot;
 
 pub async fn ask_download(filename: &str, group: &str, sender: &str) -> Result<bool> {
     let (sx, rx) = oneshot::channel::<bool>();
-
-    Notification::new()
-        .summary(&format!("{sender} shared file with {group}"))
-        .body(&format!("{filename}"))
-        .action("download", "download")
-        //.icon("firefox")
-        .show()?
-        .wait_for_action(|action| match action {
-            "download" => sx.send(true).unwrap(),
-            other => {
-                println!("action: {other}");
-                sx.send(false).unwrap()
-            }
-        });
-
+    #[cfg(target_os = "linux")]
+    {
+        Notification::new()
+            .summary(&format!("{sender} shared file with {group}"))
+            .body(&format!("{filename}"))
+            .action("download", "download")
+            //.icon("firefox")
+            .show()?
+            .wait_for_action(|action| match action {
+                "download" => sx.send(true).unwrap(),
+                other => {
+                    println!("action: {other}");
+                    sx.send(false).unwrap()
+                }
+            });
+    }
+    #[cfg(not(target_os="linux"))] {
+        Notification::new()
+            .summary(&format!("{sender} shared file with {group}"))
+            .body(&format!("{filename}"))
+            .action("download", "download")
+            //.icon("firefox")
+            .show()?;
+        sx.send(true).unwrap();
+    }
     Ok(rx.await?)
 }
 
