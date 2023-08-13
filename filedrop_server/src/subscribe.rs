292e216820e5832ff82cbb5c2799d38e486f4ee8
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2023-08-13 16:58:34 +0200

    ping hotfix

--- filedrop_server/src/subscribe.rs
+++ filedrop_server/src/subscribe.rs
@@ -50,44 +50,58 @@ pub async fn event_respond(
     });
 
     while let Some(event) = event_rx.recv().await {
-        if event.filename != "//PING" {
-            println!("{event:#?}");
-        }
-
-        //Omdan til en SSE event
         let Ok(sse_event) = Event::default().json_data(event.clone()) else {
             println!("Failed to parse {event:?} into valid sse event");
             continue;
         };
 
+        //Omdan til en SSE event
+
         //Når den modtager en event. Skal den tjekke om der er kommet nye subscribers
         while let Ok((sub, user_id)) = sub_rx.try_recv() {
             subcribers.insert(user_id, sub);
         }
+        if event.filename != "//PING" {
+            println!("{event:#?}");
 
-        //Get recievers
-        let users: Vec<Uuid> = match sqlx::query!(
-            "select user_id from group_members where group_id = $1",
-            &event.group_id.to_string()
-        )
-        .fetch_all(db::get())
-        .await
-        {
-            Ok(users) => users
-                .into_iter()
-                .filter_map(|u| Uuid::from_str(&u.user_id).ok())
-                .collect(),
-            Err(error) => {
-                println!("Failed fetching group users. Ignoring event. {error}");
-                continue;
-            }
-        };
-
-        for user_id in users {
-            if let Some(sub) = subcribers.get_mut(&user_id) {
-                if sub.start_send(Ok(sse_event.clone())).is_err() {
-                    subcribers.remove(&user_id);
+            //Get recievers
+            let users: Vec<Uuid> = match sqlx::query!(
+                "select user_id from group_members where group_id = $1",
+                &event.group_id.to_string()
+            )
+            .fetch_all(db::get())
+            .await
+            {
+                Ok(users) => users
+                    .into_iter()
+                    .filter_map(|u| Uuid::from_str(&u.user_id).ok())
+                    .collect(),
+                Err(error) => {
+                    println!("Failed fetching group users. Ignoring event. {error}");
+                    continue;
                 }
+            };
+
+            for user_id in users {
+                if let Some(sub) = subcribers.get_mut(&user_id) {
+                    if sub.start_send(Ok(sse_event.clone())).is_err() {
+                        subcribers.remove(&user_id);
+                    }
+                }
+            }
+        } else {
+            //Ping everyone still connected
+            let mut failed = vec![];
+            for (user_id, sub) in &mut subcribers {
+                if sub.start_send(Ok(sse_event.clone())).is_err() {
+                    failed.push(*user_id);
+                }
+            }
+            for user_id in failed {
+                subcribers.remove(&user_id);
+                
+                #[cfg(debug_assertions)]
+                println!("Removed {user_id}");
             }
         }
     }
